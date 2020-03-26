db3a4a7078a6d25166f2cb8ec6703c8f87547a0b
Anirudh <37629187+Anirudh-R@users.noreply.github.com>
2020-03-26 21:05:32 +0530

    Create sim_environment.py

---
+++ src/sim_environment.py
@@ -0,0 +1,5 @@
+# Acts as the environment for the RL problem.
+# Uses TraCI to monitor/control SUMO simulations. Takes the given action provided and outputs the 
+# Next state, Reward, and Statistics related to congestion/delays for plotting.
+
+# To be handled by Anirudh
