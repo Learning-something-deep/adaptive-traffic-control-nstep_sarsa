1db9d7246d63b6ac7437a99f76615b8723dce34a
Anirudh <37629187+Anirudh-R@users.noreply.github.com>
2020-03-26 22:13:02 +0530

    Create sarsa_nstep.py

---
+++ src/sarsa_nstep.py
@@ -0,0 +1,5 @@
+# Implements Differential semi-gradient n-step Sarsa for estimating q = q*
+# Implemented according to pseudo-code given in Sec 10.5 of Sutton & Bartos's RL book
+# Takes as input No. of runs, n, alpha, beta, epsilon. Outputs the final weights after all the runs.
+
+# Handled by Mithun
