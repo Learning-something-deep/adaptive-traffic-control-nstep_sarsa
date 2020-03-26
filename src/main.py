cca5e568bda2144d7edbcc2633cb03167b2ede05
Anirudh <37629187+Anirudh-R@users.noreply.github.com>
2020-03-26 22:44:57 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,6 @@
+# Main module; compares the performance of n-step Sarsa and LQF algorithms by plotting various metrics
+# Evaluates the effect of changing the parameters of n-step Sarsa to find optimal values
 
+# Depends on: sarsa_nstep.py, lqf_algo.py, plot_metrics.py
+
+# Handled by All
