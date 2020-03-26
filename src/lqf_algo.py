92107ab4f0b6c0f4d65dc5e747a23a481f38e00b
Anirudh <37629187+Anirudh-R@users.noreply.github.com>
2020-03-26 22:36:37 +0530

    Update lqf_algo.py

--- src/lqf_algo.py
+++ src/lqf_algo.py
@@ -1,4 +1,6 @@
 # Implements the Longest Queue First (LQF) algorithm on the traffic network intersections
 # Takes as input No. of runs
 
+# Depends on: sim_environment.py, plot_metrics.py
+
 # Handled by Mithun
