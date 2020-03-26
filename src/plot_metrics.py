f9a7504f074922a8f4a9bb369801f47adb693de9
Anirudh <37629187+Anirudh-R@users.noreply.github.com>
2020-03-26 22:21:04 +0530

    Create plot_metrics.py

---
+++ src/plot_metrics.py
@@ -0,0 +1,4 @@
+# Parses the output of SUMO simulation, and plots congestion-related performance metrics.
+# Plots Average queue length at intersections, Average delay, and Average stopped delay (averaged across simulation runs)
+
+# Handled by Akshay
