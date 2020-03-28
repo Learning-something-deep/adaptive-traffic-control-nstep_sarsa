debdc8d501e15fa7a625201b648c85d98b7c0b14
Anirudh <37629187+Anirudh-R@users.noreply.github.com>
2020-03-28 16:06:59 +0530

    Update plot_metrics.py

--- src/plot_metrics.py
+++ src/plot_metrics.py
@@ -1,7 +1,36 @@
 # Parses the output of SUMO simulation, and plots congestion-related performance metrics.
-# Plots Average queue length at intersections, Average delay, and Average stopped delay (averaged across simulation runs)
-# Uses the output dump file of SUMO for each run as input
+# Plots Average queue length at intersections, Average arrival time, Average waiting time, and Average time loss (averaged 
+# across simulation runs). Uses the output dump file of SUMO for each run as input.
 
 # Depends on: None
 
-# Handled by Akshay
+# Handled by Mithun
+
+# Desc: Clears any existing data for all runs, and starts fresh.
+# Inputs - None
+# Outputs - None
+def init():
+
+
+    return
+
+
+
+# Desc: Parses the simulation output file at the end of run. Performs averaging, and stores various vehicle metrics
+#       for the run.
+# Inputs - None
+# Outputs - None
+def record_metrics_of_run():
+
+
+    return
+
+
+
+# Desc: Plots all the metrics, averaged across runs, vs time.
+# Inputs - None
+# Outputs - None
+def plot_all_metrics():
+
+
+    return
