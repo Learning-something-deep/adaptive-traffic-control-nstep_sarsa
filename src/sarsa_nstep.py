38980318bf55d699cd5c82046fc01143428aa016
Anirudh <37629187+Anirudh-R@users.noreply.github.com>
2020-03-28 16:08:18 +0530

    Update sarsa_nstep.py

--- src/sarsa_nstep.py
+++ src/sarsa_nstep.py
@@ -4,4 +4,37 @@
 
 # Depends on: sim_environment.py
 
-# Handled by Mithun
+# Handled by Akshay
+
+
+# Desc: Runs the Differential n-step Sarsa algorithm for estimating optimal action-value function using Linear
+#       function approximation.
+# Inputs - n: n-step bootstrapping
+#          alpha: step size for weight updates
+#          beta: step size of avg reward updates
+#          epsilon: exploration parameter for epsilon-greedy
+#          Nruns: No. of runs
+# Outputs - W: Trained weights after all runs
+def sarsa_nstep_diff(n, alpha, beta, epsilon, Nruns):
+
+    # Note: functions used by this function
+    # sim_environment.start_new_run(run)
+    # sim_environment.take_action(a)
+    # sim_environment.get_current_state()
+    # q_est(s, a, w)
+
+    return W
+
+
+
+# Desc: Computes the approximate action-value function q(s, a) using Linear function approximation.
+# Inputs - s: 15-element array containing the no. of vehicles waiting at the intersections in state s
+#          a: Action taken in state s, 1<=a<=15
+#          w: 556-element weight vector, with the last element corresponding to bias
+# Outputs - q_val: Calculated q(s, a) value
+def q_est(s, a, w):
+
+
+
+    return q_val
+
