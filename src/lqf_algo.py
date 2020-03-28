1dec661c4e99780bb78c2f79f9b932aafb7f6328
Anirudh <37629187+Anirudh-R@users.noreply.github.com>
2020-03-28 16:09:37 +0530

    Update lqf_algo.py

--- src/lqf_algo.py
+++ src/lqf_algo.py
@@ -3,4 +3,18 @@
 
 # Depends on: sim_environment.py
 
-# Handled by Mithun
+# Handled by Akshay
+
+
+# Desc: Runs Longest Queue First(LQF) algorithm on each intersection of the traffic network.
+# Inputs - Nruns: No. of runs
+# Outputs - None
+def lqf(Nruns):
+
+    # Note: functions used by this function
+    # sim_environment.start_new_run(run)
+    # sim_environment.take_action(a)
+    # sim_environment.get_current_state()
+
+    return
+
