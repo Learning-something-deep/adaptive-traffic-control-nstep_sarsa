2bb0e5ca8b7fe5a9cc56148340694484815580e4
Anirudh <37629187+Anirudh-R@users.noreply.github.com>
2020-04-04 21:13:44 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -4,3 +4,14 @@
 # Depends on: sarsa_nstep.py, lqf_algo.py, plot_metrics.py
 
 # Handled by All
+
+
+import sim_environment
+import sarsa_nstep
+
+
+# Estimate q* using n-step SARSA
+sim_environment.endis_sumo_guimode(1)   # SUMO GUI mode
+n = 6; c = 5; epsilon = 0.6; Nruns = 1
+W = sarsa_nstep.sarsa_nstep_diff_train(n, c, epsilon, Nruns)
+print(W)
