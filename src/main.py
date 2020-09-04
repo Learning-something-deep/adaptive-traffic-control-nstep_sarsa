c674d1ca83ffe571ae95384c5d0f0ffc3860f0f6
akshaykekuda <kekuda.akshay@gmail.com>
2020-09-04 12:09:33 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,7 +3,6 @@
 
 # Depends on: sarsa_nstep.py, lqf_algo.py, static_signalling.py, plot_metrics.py
 
-# Handled by All
 
 
 import sim_environment
@@ -13,31 +12,31 @@ import sarsa_nstep
 import plot_metrics
 from matplotlib import pyplot as plt
 
+print("Start of Simulation")
 
 # Static signalling
 sim_environment.endis_sumo_guimode(1)
-Nruns = 1
+Nruns = 150
 static_signalling.static_signalling(Nruns)
 plot_metrics.plot_all_metrics("Static signalling")
 
-
 # Longest Queue First (LQF) algorithm
 sim_environment.endis_sumo_guimode(1)
-Nruns = 1
+Nruns = 150
 lqf_algo.lqf(Nruns)
 plot_metrics.plot_all_metrics("LQF algo")
 
-
 # Estimate q* using n-step SARSA
 sim_environment.endis_sumo_guimode(1)
-n = 4; c = 0.1; epsilon = 1.0; Nruns = 5
+n = 3
+c = 2
+epsilon = 1.0
+Nruns = 30
 W = sarsa_nstep.sarsa_nstep_diff_train(n, c, epsilon, Nruns)
 
 # Try out performance with the estimated weights
 sim_environment.endis_sumo_guimode(1)
-Nuns = 3
-sarsa_nstep.sarsa_nstep_diff_live(W, Nuns)
-plot_metrics.plot_all_metrics("Sarsa algo")
-
-
-plt.show()      # to prevent figures from closing
+Nruns = 150
+sarsa_nstep.sarsa_nstep_diff_live(W, Nruns)
+plot_metrics.plot_all_metrics("Sarsa" + " n=" + str(n) + "c= " + str(c))
+plt.show()  # to prevent figures from closing
