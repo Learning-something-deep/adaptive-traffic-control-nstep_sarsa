28542e3f1376cf13ffb0db5b9b40d339431e2036
akshaykekuda <kekuda.akshay@gmail.com>
2020-09-04 12:11:47 +0530

    Update sim_environment.py

--- src/sim_environment.py
+++ src/sim_environment.py
@@ -3,8 +3,6 @@
 
 # Depends on: plot_metrics.py
 
-# Handled by Anirudh
-
 
 from sumolib import checkBinary
 import traci
@@ -13,14 +11,15 @@ import array
 import os
 import platform
 
-T_SWITCH = 12                                               # Intersection switching time
-T_TRANS = 4                                                 # Signal transition time when changing sides
-OCC_TH_HIGH = 70.0                                          # Detector occupancy high threshold for jamming
-OCC_TH_LOW = 30.0                                           # Detector occupancy low threshold for jamming
-NUM_INTN_SIDES = 15                                         # No. of intersection sides
-NUM_LANES = [3, 2, 3, 2, 3, 2, 3, 2, 1, 1, 1, 1, 2, 1, 2]   # No. of lanes at intersection sides
+T_SWITCH = 12  # Intersection switching time
+T_TRANS = 4  # Signal transition time when changing sides
+OCC_TH_HIGH = 70.0  # Detector occupancy high threshold for jamming
+OCC_TH_LOW = 30.0  # Detector occupancy low threshold for jamming
+NUM_INTN_SIDES = 15  # No. of intersection sides
+NUM_LANES = [3, 2, 3, 2, 3, 2, 3, 2, 1, 1, 1, 1, 2, 1, 2]  # No. of lanes at intersection sides
+JAMLEN_THRESH = 65  # Length of vehicle queue for jamming
 
-TL_IDS = ["gneJ20", "gneJ18", "151147259", "gneJ69"]        # Intersection traffic light IDs
+TL_IDS = ["gneJ20", "gneJ18", "151147259", "gneJ69"]  # Intersection traffic light IDs
 
 # Intersection phases
 TL_PHASES = {'N1_G': 0, 'N1_Y': 1, 'N2_G': 2, 'N2_Y': 3, 'N3_G': 4, 'N3_Y': 5, 'N4_G': 6, 'N4_Y': 7,
@@ -31,12 +30,12 @@ TL_PHASES = {'N1_G': 0, 'N1_Y': 1, 'N2_G': 2, 'N2_Y': 3, 'N3_G': 4, 'N3_Y': 5, '
 QLEN_FILE = "../scripts/qlengths.xml"
 qlenfile = open(QLEN_FILE, 'w')
 
-sumoBinary = checkBinary('sumo-gui')                        # mode of SUMO
+sumoBinary = checkBinary('sumo-gui')  # mode of SUMO
 
-intn_prev_action = [1, 5, 9, 13]                            # previous action taken at the intersections
+intn_prev_action = [1, 5, 9, 13]  # previous action taken at the intersections
 
-first_act_of_run = 0                                        # flag for occurrence of first action of a run
-skip_time = 0                                               # time spent in initial randomization
+first_act_of_run = 0  # flag for occurrence of first action of a run
+skip_time = 0  # time spent in initial randomization
 t = 0
 
 curr_state = array.array('f', [0] * NUM_INTN_SIDES)
@@ -49,7 +48,6 @@ prev_jam_state = array.array('i', [0] * NUM_INTN_SIDES)
 # Inputs - run: run number, 0 implies first run
 # Outputs - None
 def start_new_run(run):
-
     global curr_state, prev_state, curr_jam_state, prev_jam_state, intn_prev_action, first_act_of_run, skip_time, qlenfile
 
     curr_state = array.array('f', [0] * NUM_INTN_SIDES)
@@ -66,16 +64,18 @@ def start_new_run(run):
 
     if platform.system() == 'Windows':
         # Generate a new random routes file
-        os.system("python \"%SUMO_HOME%/tools/randomTrips.py\" -n ../scripts/txmap.net.xml --trip-attributes=\"type=\\\"light_norm_heavy\\\"\" "
-            "-a ../scripts/txmap.add.xml -r ../scripts/txmap.rou.xml -e 12000 -p 1.25 --binomial=5 -L")
+        os.system(
+            "python \"%SUMO_HOME%/tools/randomTrips.py\" -n ../scripts/txmap.net.xml --trip-attributes=\"type=\\\"light_norm_heavy\\\"\" "
+            "-a ../scripts/txmap.add.xml -r ../scripts/txmap.rou.xml -e 12000 -p 0.75 --binomial=5 -L")
         # Delete unwanted alt route file
         os.system("del \"../scripts/txmap.rou.alt.xml\"")
         # Delete unwanted trips file
         os.system("del trips.trips.xml")
     elif platform.system() == 'Linux':
         # Generate a new random routes file
-        os.system("python \"$SUMO_HOME/tools/randomTrips.py\" -n ../scripts/txmap.net.xml --trip-attributes=\"type=\\\"light_norm_heavy\\\"\" "
-            "-a ../scripts/txmap.add.xml -r ../scripts/txmap.rou.xml -e 12000 -p 1.25 --binomial=5 -L")
+        os.system(
+            "python \"$SUMO_HOME/tools/randomTrips.py\" -n ../scripts/txmap.net.xml --trip-attributes=\"type=\\\"light_norm_heavy\\\"\" "
+            "-a ../scripts/txmap.add.xml -r ../scripts/txmap.rou.xml -e 12000 -p 0.75 --binomial=5 -L")
         # Delete unwanted alt route file
         os.system("rm \"../scripts/txmap.rou.alt.xml\"")
         # Delete unwanted trips file
@@ -96,10 +96,9 @@ def start_new_run(run):
 # Desc: Gives Green signal to the given intersection side.
 # Inputs - a: The intersection side to be given Green signal to, 1<=a<=15. If a = 0, no action is taken
 # Outputs - A dictionary containing 2 elements
-#               'rwd': Reward; Returns -100 if the current simulation is over
+#               'rwd': Reward; Returns 1000 if the current simulation is over
 #               'next_state': 15-element array containing the no. of vehicles waiting at the intersections in new state
 def take_action(a):
-
     global curr_state, prev_state, prev_jam_state, intn_prev_action, first_act_of_run, skip_time, t
 
     # randomization phase, no control; let static TL logic of SUMO run
@@ -116,14 +115,14 @@ def take_action(a):
             qlenfile.write("</qlengths>")
             qlenfile.close()
             plot_metrics.record_metrics_of_run(skip_time)
-            return {'rwd': -100, 'next_state': curr_state}
+            return {'rwd': 1000, 'next_state': curr_state}
 
         prev_state = curr_state
         prev_jam_state = curr_jam_state
         return {'rwd': R, 'next_state': curr_state}
     # randomization phase over
     elif first_act_of_run == 0:
-        t = 0           # time counting starts from here
+        t = 0  # time counting starts from here
         first_act_of_run = 1
 
     # Intersection being controlled
@@ -138,12 +137,13 @@ def take_action(a):
 
     # if action same as previous for this intersection
     if a == intn_prev_action[intn]:
-        traci.trafficlight.setPhase(TL_IDS[intn], TL_PHASES['N' + str(a) + '_G'])   # Continue Green during transition
+        traci.trafficlight.setPhase(TL_IDS[intn], TL_PHASES['N' + str(a) + '_G'])  # Continue Green during transition
         for i in range(T_TRANS):
             traci.simulationStep()
     # different action
     else:
-        traci.trafficlight.setPhase(TL_IDS[intn], TL_PHASES['N' + str(intn_prev_action[intn]) + '_Y'])   # Give Yellow during transition
+        traci.trafficlight.setPhase(TL_IDS[intn], TL_PHASES[
+            'N' + str(intn_prev_action[intn]) + '_Y'])  # Give Yellow during transition
         for i in range(T_TRANS):
             traci.simulationStep()
 
@@ -164,7 +164,7 @@ def take_action(a):
         qlenfile.write("</qlengths>")
         qlenfile.close()
         plot_metrics.record_metrics_of_run(skip_time)
-        return {'rwd': -100, 'next_state': curr_state}
+        return {'rwd': 1000, 'next_state': curr_state}
 
     R = calculate_reward(curr_state, prev_state, a)
 
@@ -179,7 +179,6 @@ def take_action(a):
 # Inputs - None
 # Outputs - state: 15-element array containing the detector occupancies at the intersections currently
 def get_current_state():
-
     global curr_jam_state
 
     state = array.array('f', [0] * NUM_INTN_SIDES)
@@ -187,7 +186,7 @@ def get_current_state():
     for i in range(NUM_INTN_SIDES):
         n = 0.0
         for j in range(NUM_LANES[i]):
-            n = max(n, traci.lanearea.getLastStepOccupancy("e2det_N"+str(i+1)+"_"+str(j)))
+            n = max(n, traci.lanearea.getLastStepOccupancy("e2det_N" + str(i + 1) + "_" + str(j)))
 
         state[i] = n
 
@@ -199,70 +198,65 @@ def get_current_state():
     return state
 
 
+# Desc: Returns the no. of vehicles waiting at the intersections currently.
+# Inputs - None
+# Outputs - state: 15-element array containing the no. of vehicles waiting at the intersections currently
+def get_current_qlen_state():
+    qlen_state = array.array('i', [0] * NUM_INTN_SIDES)
+
+    for i in range(NUM_INTN_SIDES):
+        n = 0
+        jammed = 0
+        for j in range(NUM_LANES[i]):
+            n = max(n, traci.lanearea.getJamLengthVehicle("e2det_N" + str(i + 1) + "_" + str(j)))
+            if traci.lanearea.getJamLengthMeters("e2det_N" + str(i + 1) + "_" + str(j)) > JAMLEN_THRESH:
+                jammed = 1
+
+        if jammed == 1:
+            qlen_state[i] = int(n * 1.5 + 1)  # if jammed, over-estimate possible queue length
+        else:
+            qlen_state[i] = n
+
+    return qlen_state
+
+
 # Desc: Calculates the reward when transitioning states.
 # Inputs - curr_occ: Current state of congestion
 #          prev_occ: Previous state of congestion
 #          a: Action taken for going from prev to curr state
 # Outputs - Reward: Reward
 def calculate_reward(curr_occ, prev_occ, a):
-
     Reward = 0
+    state_qlen = get_current_qlen_state()
 
     # static signalling; reward doesn't matter
     if a == 0:
         return 0
 
+    # check the intersection to operate
     if a < 5:
         a_space = [1, 2, 3, 4]
+        intn = 0
     elif a < 9:
         a_space = [5, 6, 7, 8]
+        intn = 1
     elif a < 13:
         a_space = [9, 10, 11, 12]
+        intn = 2
     else:
         a_space = [13, 14, 15]
+        intn = 3
 
-    if curr_occ[a - 1] < OCC_TH_LOW and prev_occ[a - 1] < OCC_TH_LOW:
-        Reward = -40
-    elif curr_occ[a - 1] >= OCC_TH_LOW and prev_occ[a - 1] < OCC_TH_LOW:
-        Reward = -20
-    elif curr_occ[a - 1] < OCC_TH_LOW and (OCC_TH_LOW <= prev_occ[a - 1] < OCC_TH_HIGH):
-        prev_occ_arr = []
-        for a_temp in a_space:
-            prev_occ_arr.append(prev_occ[a_temp - 1])
-        prev_occ_max = max(prev_occ_arr)
-        if prev_occ_max == prev_occ[a - 1]:
-            Reward = 10
-        else:
-            Reward = 5
-    elif curr_occ[a - 1] >= OCC_TH_LOW and (OCC_TH_LOW <= prev_occ[a - 1] < OCC_TH_HIGH):
-        prev_occ_arr = []
-        for a_temp in a_space:
-            prev_occ_arr.append(prev_occ[a_temp - 1])
-        prev_occ_max = max(prev_occ_arr)
-        if prev_occ_max == prev_occ[a - 1]:
-            Reward = 20
-        else:
-            Reward = 15
-    elif curr_occ[a - 1] < OCC_TH_LOW and prev_occ[a - 1] >= OCC_TH_HIGH:
-        prev_occ_arr = []
-        for a_temp in a_space:
-            prev_occ_arr.append(prev_occ[a_temp - 1])
-        prev_occ_max = max(prev_occ_arr)
-        if prev_occ_max == prev_occ[a - 1]:
-            Reward = 30
-        else:
-            Reward = 25
-    elif curr_occ[a - 1] >= OCC_TH_LOW and prev_occ[a - 1] >= OCC_TH_HIGH:
-        Reward = 40
+    for i in a_space:
+        Reward += state_qlen[i - 1]
 
-    return Reward
+    return -Reward
 
 
 # Desc: Enable/Disable GUI mode of SUMO
 # Inputs - mode: 1 = GUI mode, 0 = non-GUI mode
 # Outputs - None
 def endis_sumo_guimode(mode):
-
     global sumoBinary
 
     if mode == 1:
